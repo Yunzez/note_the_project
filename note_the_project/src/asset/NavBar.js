04118c3f89f54cd377c13159c3d3cbe56aaff2bc
yunze <yunzez@uw.edu>
2022-01-03 23:28:49 -0800

    side bar update, temporary animation

--- note_the_project/src/asset/NavBar.js
+++ note_the_project/src/asset/NavBar.js
@@ -8,30 +8,55 @@ import './NavBar.css';
 //import Mui and Font awesome, we will be mainly using this ui
 import * as MuiMaterial from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import * as FaiSolid  from '@fortawesome/free-solid-svg-icons'
+import * as FaiSolid from '@fortawesome/free-solid-svg-icons'
 //type rfce to set up the function like this 
 
 
 function NavBar(props) {
+    function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+
     const [sidebar, setSidebar] = useState(false)
-    const showSidebar = () => setSidebar(!sidebar)
+    const showSidebar = async() => {
+        setSidebar(!sidebar)
+        
+        // let element = document.getElementsByClassName('nav-menu active')
+        // if (element.length == 0) {
+        //     let titles = document.querySelectorAll('.item-title');
+        //     console.log(titles)
+        //     //  await delay(1000);
+        //     titles.forEach((title)=>{
+        //         title.classList.add('d-inline');
+        //         title.classList.remove('d-none');
+        //     })
+            
+        // } else {
+        //     let titles = document.querySelectorAll('.item-title');
+        //     titles.forEach((title)=>{
+        //         title.classList.add('d-none');
+        //         title.classList.remove('d-inline');
+        //     })
+        // }
+    }
     return (
         <nav>
             <div className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <div className={sidebar ? 'nav-menu-items active' : 'nav-menu-items'} onMouseLeave={showSidebar} onMouseEnter={showSidebar}>
                     <div className='navbar-toggle '>
                         <Link to='/' className='menu-bars d-flex flex-row-reverse mb-2 me-2 mt-2'>
-                        <FontAwesomeIcon id='open-menu-arrow' icon={FaiSolid.faArrowRight} size="md"/>
-                        <FontAwesomeIcon  id='close-menu-arrow' icon={FaiSolid.faArrowLeft}size="md"/>
+                            <FontAwesomeIcon id='open-menu-arrow' icon={FaiSolid.faArrowRight} />
+                            <FontAwesomeIcon id='close-menu-arrow' icon={FaiSolid.faArrowLeft}  />
                         </Link>
+                        <MuiMaterial.Divider className='mb-3'/>
                     </div>
                     {SiderbarInfo.map((line, index) => {
                         return (
                             <div key={index} className={line.className}>
-                                <Link to={line.path}>
-                                    {line.icon}
+                                <Link to={line.path} className='d-flex justify-content-start'>
+                                    <div className='item-icon'>{line.icon }</div>
                                     <span className='item-title'>{line.title}</span>
-                                    <MuiMaterial.Divider />
+                                    <div className="item-place-holder"></div>
                                 </Link>
 
                             </div>
