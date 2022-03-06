7505f9631ef1b95a71a604739b9046a9bd31cfed
yunzez <yunzez@uw.edu>
2022-03-05 17:35:20 -0800

    update widgets

--- src/asset/RenderColumn.js
+++ src/asset/RenderColumn.js
@@ -1,19 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './columnstyle.css'
 import * as MuiMaterial from '@mui/material';
 import RenderColumnTrigger from './RenderColumnTrigger';
 function RenderColumn(props) {
     let name = props.name;
-    if (name == ''){
+    const [titleInput, setTitleInput] = useState('')
+    const [widgetList, setWidgetList] = useState([])
+
+    if (name == '') {
         name = 'Untitle'
     }
+
+    function handleAddWidget() {
+        console.log('adding widget')
+        document.getElementsByClassName('addWidgetButton')[0].classList.toggle('d-none')
+        document.getElementsByClassName('widgetTitle')[0].classList.toggle('d-none')
+    }
+
+    function handleGoWidget() {
+        setTitleInput(document.getElementsByClassName('input-title')[0].value)
+        console.log('creating widget', titleInput)
+        var newWidget = {
+            title: titleInput,
+            type: 0,
+            content: {}
+        }
+        console.log(titleInput)
+        var temp = []
+        if (widgetList.length > 0) {
+            widgetList.map((item, index) => {
+                temp.push(item)
+            })
+            temp.push(newWidget)
+        } else {
+            temp.push(newWidget)
+        }
+        setWidgetList(temp)
+        setTitleInput('')
+        document.getElementsByClassName('input-title')[0].value = ''
+        document.getElementsByClassName('addWidgetButton')[0].classList.toggle('d-none')
+        document.getElementsByClassName('widgetTitle')[0].classList.toggle('d-none')
+        
+    }
+
+
+    console.log(widgetList)
+
+
     return (
         <div className='column p-2'>
             <p>{name}</p>
-            <a className="btn mx-auto add-widget" >Add a widget</a>
+            <div>
+                {widgetList.map((item, index) => {
+                    console.log('updae widget')
+                    return (
+                        <div className='widget bg-white p-1 rounded mb-2'>
+                            <p>{item.title}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <a className="addWidgetButton btn mx-auto add-widget" onClick={() => { handleAddWidget() }}>Add a widget</a>
 
+            <div className='d-none widgetTitle'>
+                <div className="form-group ">
+                    <input className="form-control mb-1 input-title" aria-describedby="title" placeholder="Widget Title"
+                        onChange={
+                            () => { setTitleInput(document.getElementsByClassName('input-title')[0].value) }}></input>
+                </div>
+                <button className="btn btn-primary" onClick={() => { handleGoWidget() }}>Go</button>
+            </div>
+
+           
         </div>
-        
+
 
     )
 }
