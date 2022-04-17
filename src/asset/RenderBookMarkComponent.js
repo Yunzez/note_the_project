e326bcefc99b28d15f90cadbb0e2b0c7350cbfad
yunzez <yunzez@uw.edu>
2022-04-16 17:58:57 -0700

    started web

--- src/asset/RenderBookMarkComponent.js
+++ src/asset/RenderBookMarkComponent.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+function RenderBookMarkComponent(props) {
 
-function RenderBookMarkComponent() {
-  return (
-    <div>RenderBookMarkComponent</div>
-  )
+    const [input, setInput] = useState('');
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [pureURL, setPureURL] = useState('')
+    var pos = props.pos
+    var setupID = 'normaltext-setup' + pos;
+    var showID = 'normaltext-show' + pos;
+    var inputID = 'component-normal-textinput' + pos;
+
+
+   
+
+
+
+    return (
+        <div className="card p-2">
+            <label for="basic-url">Website URL:</label>
+            <div class="input-group">
+                <input type="text" class="form-control" id={inputID} input={input} onChange={() => { setInput(document.getElementById(inputID).value) }} aria-describedby="basic-addon3"></input>
+            </div>
+            <div className='btn btn-primary mt-1' onClick={() => { GetWebData() }}>Generate Bookmark</div>
+        </div>
+    )
+
+
+    function GetWebData() {
+        var currInput = input;
+        setPureURL(currInput.replace('', ' '));
+        console.log(currInput);
+        
+
+    }
 }
 
 export default RenderBookMarkComponent
