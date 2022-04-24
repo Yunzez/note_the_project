c5ad71d1f27355c4f7f25d66e08cfb4877a383be
yunzez <yunzez@uw.edu>
2022-04-23 20:35:49 -0700

    api update

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,7 +2,15 @@ var express = require('express')
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-    res.send("API is working, testing")
+    res.render('hello, testing');
+    // send a server error with text
+    // res.status(500).json({message: "Error"})
+
+    // send a normal json
+    // res.json({message: "Error"})
+
+    // download a file
+    // res.download("testAPI.js")
 })
 
 module.exports = router
