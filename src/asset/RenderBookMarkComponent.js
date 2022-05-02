61fdf17057fe3a96c9e9d5a90e0dacbc8df55334
yunze <yunzez@uw.edu>
2022-05-01 21:02:25 -0700

    webbook

--- src/asset/RenderBookMarkComponent.js
+++ src/asset/RenderBookMarkComponent.js
@@ -6,32 +6,41 @@ function RenderBookMarkComponent(props) {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [pureURL, setPureURL] = useState('')
+
     var pos = props.pos
     var setupID = 'normaltext-setup' + pos;
     var spinID = 'spin' + pos;
-    var hidetextID ="hidetext" + pos;
+    var hidetextID = "hidetext" + pos;
     var inputID = 'component-normal-textinput' + pos;
 
 
     const [items, setItems] = useState([]);
-  
+
     // Note: the empty deps array [] means
     // this useEffect will run once
     // similar to componentDidMount()
-  
+
 
     return (
-        <div className="card p-2">
-            <label for="basic-url">Website URL:</label>
-            <div class="input-group">
-                <input type="text" class="form-control" id={inputID} input={input} onChange={() => { setInput(document.getElementById(inputID).value) }} aria-describedby="basic-addon3"></input>
-            </div>
-            <div className='btn btn-primary mt-1' onClick={() => { GetWebData() }}>
-                <div id={spinID}  className="d-none">
-                <ClipLoader size={50} />
+        <React.Fragment>
+            {isLoaded ? (
+                <div>{items.des}</div>
+            ) : (
+                <div className="card p-2">
+                <label for="basic-url">Website URL:</label>
+                <div class="input-group">
+                    <input type="text" class="form-control" id={inputID} input={input} onChange={() => { setInput(document.getElementById(inputID).value) }} aria-describedby="basic-addon3"></input>
                 </div>
-            <p id={hidetextID}>Generate Bookmark</p></div>
-        </div>
+                <div className='btn btn-primary mt-1' onClick={() => { GetWebData() }}>
+                    <div id={spinID} className="d-none">
+                        <ClipLoader size={50} />
+                    </div>
+                    <p id={hidetextID}>Generate Bookmark</p></div>
+            </div>
+                
+            )}
+
+        </React.Fragment>
     )
 
 
@@ -40,28 +49,28 @@ function RenderBookMarkComponent(props) {
         document.getElementById(hidetextID).classList.toggle('d-none')
         var currInput = input;
         setPureURL(currInput.replace(' ', ''));
-        var testlink = "http://localhost:3001/user/getwebmeta/?url=" + pureURL
-        var fulllink= "https://note-the-project-n.herokuapp.com/user/getwebmeta?url=" + pureURL
+        var fulllink = "http://localhost:3001/user/getwebmeta/?url=" + pureURL
+        // var fulllink= "https://note-the-project-n.herokuapp.com/user/getwebmeta?url=" + pureURL
         fulllink = fulllink.replace(' ', '')
         console.log(fulllink)
         fetch(fulllink)
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result);
-            console.log(items)
-            document.getElementById(hidetextID).classList.toggle('d-none')
-            document.getElementById(spinID).classList.toggle('d-none')
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                    console.log(items)
+                    document.getElementById(hidetextID).classList.toggle('d-none')
+                    document.getElementById(spinID).classList.toggle('d-none')
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
 
     }
 }
