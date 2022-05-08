8b63dce25a5425a822ca33984d52808d6d4f9f59
Jason Li <56493094+lzw365-code@users.noreply.github.com>
2022-05-07 17:06:33 -0700

    music

--- src/asset/RenderMusicPlayerComponent.js
+++ src/asset/RenderMusicPlayerComponent.js
@@ -7,11 +7,13 @@ import autosize from 'autosize';
 import TextArea from './AutoText'
 import { Progress } from 'reactstrap';
 import { checkActionCode } from '@firebase/auth';
+import './musicstyle.css';
 
 
 function RenderMusicPlayerComponent(props) {
     const [normalInput, setNormalInput] = useState('');
     const [song, setSong] = useState(undefined);
+    const [more, setMore] = useState(0)
     var pos = props.pos
     var setupID = 'normaltext-setup' + pos;
     var showID = 'normaltext-show' + pos;
@@ -20,7 +22,11 @@ function RenderMusicPlayerComponent(props) {
 
     function search() {
         var musicList = []
-        fetch("https://itunes.apple.com/search?term=" + normalInput + "&media=music")
+        setMore(0)
+        if (!state.previewAudio.paused) {
+            state.previewAudio.pause();
+        }
+        fetch("https://itunes.apple.com/search?term=" + normalInput.trim())
             .then(response => response.json())
             .then(data => {
                 data.results.forEach(element => {
@@ -60,16 +66,46 @@ function RenderMusicPlayerComponent(props) {
                     No results found, please try again
                 </div>
             )
+        } else if (song.length > 5 && more == 0) {
+            const song5 = song.slice(0, 5)
+            return (
+                <div>
+                    <div className="d-flex flex-column">
+                        {song5.map((item, index) => {
+                            return (
+                                <div className='d-flex flex-row border mb-1 p-1'>
+                                    <img className='musicCover' src={item.artworkUrl100} alt={item.trackName} title={item.trackName} onClick={() => { playAudio(item) }} />
+                                    <div className='ps-2'>
+                                        <div>Name: {item.trackName}</div>
+                                        <div>Artist: {item.artistName}</div>
+                                        <div>Collection: {item.collectionName}</div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className='btn btn-secondary justify-content-center' onClick={() => setMore(1)}>
+                        show more result...
+                    </div>
+                </div>
+            )
         } else {
             return (
-                <div className="d-flex flex-wrap">
-                    {song.map((item, index) => {
-                        return (
-                            <div>
-                                <img src={item.artworkUrl100} alt={item.trackName} title={item.trackName} onClick={() => { playAudio(item) }} />
-                            </div>
-                        )
-                    })}
+                <div>
+                    <div className="d-flex flex-column">
+                        {song.map((item, index) => {
+                            return (
+                                <div className='d-flex flex-row border mb-1 p-1'>
+                                    <img className='musicCover' src={item.artworkUrl100} alt={item.trackName} title={item.trackName} onClick={() => { playAudio(item) }} />
+                                    <div className='ps-2'>
+                                        <div>Name: {item.trackName}</div>
+                                        <div>Artist: {item.artistName}</div>
+                                        <div>Collection: {item.collectionName}</div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
                 </div>
             )
         }
@@ -79,7 +115,7 @@ function RenderMusicPlayerComponent(props) {
         <div>
             <div>Search for a music:</div>
             <TextArea className="note form-control " id={inputID} normalInput={normalInput} setNormalInput={setNormalInput} />
-            <button onClick={() => { search() }} className='btn add-widget mt-2'><i class="fa fa-music" aria-hidden="true"></i> Search</button>
+            <button onClick={() => { search() }} className='btn btn-primary mt-2 mb-2'><i class="fa fa-music" aria-hidden="true"></i> Search</button>
             {music()}
             {/* <form class="form-inline" method="GET" action="https://itunes.apple.com/search">
             </form> */}
